51c8f2abd2b9411a74a7fd30c9c077313bd366e5
cdh <jaehoon92@gmail.com>
2019-05-05 17:50:07 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // This is the only time we do direct assignmnet
+    // to this.state
+    state = { lat: null, errorMessage: '' };
+
+    // Called only once
+    componentDidMount(){
+        // Gets user's geolocation by Mozilla API
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                // we called setState!!!
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => {
+                this.setState({errorMessage: err.message});
+            }
+        );
+    }
+
+    renderContent() {
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+    
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+    
+        return <Spinner message="Please accept location request" />
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
